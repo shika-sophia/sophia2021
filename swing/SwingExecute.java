1201bb9caf0c697d7fb97df3553856ecf45da900
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-01-24 18:20:52 +0900

    2021-01-24-1800

--- swing/SwingExecute.java
+++ swing/SwingExecute.java
@@ -1,33 +1,15 @@
 package swing;
 
-import utility.CmdExecute;
+import utility.cmdDiv.CmdJavaExe;
 
-public class SwingExecute {
+public class SwingExecute extends CmdJavaExe{
+    public SwingExecute(){ }
 
     public SwingExecute(String className) {
-        swgExecutor(className);
+        super(className);
+        cmdJavaEclipse();
     }//constructor
 
-    public void swgExecutor(String className) {
-        String classNameCp = className
-            .replace('.', '\\').replace("$1", ".java");
-
-        //---- get current directory path ('cd')----
-        var cmdExe = new CmdExecute();
-        String cd = cmdExe.cmdExecutor("@cd");
-
-        String path = cd + "\\src\\" + classNameCp;
-
-        System.out.println("className: " + classNameCp);
-        System.out.println("path: " + path);
-
-        //---- conpile, execute ----
-        cmdExe.cmdExecutor("cd " + path);
-        String result = cmdExe.cmdExecutor(
-            "javac -encoding UTF-8 " + classNameCp);
-
-        System.out.println("result: \n" + result);
-    }//swgExecutor()
 }//class
 
 /*
