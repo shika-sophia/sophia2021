0a89746653643c26fe8feee29c6cd3b5db5b678c
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-01-25 11:16:31 +0900

    2021-01-25-1100

--- swing/SwingExecute.java
+++ swing/SwingExecute.java
@@ -10,9 +10,36 @@ public class SwingExecute extends CmdJavaExe{
         cmdJavaEclipse();
     }//constructor
 
+    public static void main(String[] args) {
+        new SwingExecute("swing.sample.SampleWindow$1");
+    }
 }//class
 
 /*
-//====== Test Result ======
-className: swing.sample.SampleWindow.java
+//====== 考察 / 2021-01-25 ======
+eclipseでこのクラスを直接実行できないときのために、
+実行したいクラスに このような main()を用意し、
+swing部分はメソッド or コンストラクタにして、
+コマンドプロンプトで実行することを考えてみたが
+
+public static void main(String[] args) {
+    if(className == null){
+        String className = new Object(){ }.getClass().getName();
+        new SwingExecute(className);
+    } else {
+        sampleWnd()
+    }
+}//main()
+
+これを記述すると
+main() -> SwingExecute -> java SampleWind -> main() とループしてしまう。
+if文を付けて メソッド呼び出し or コンストラクタ呼び出しにしても同様
+
+「自動でコマンドプロンプト実行」という発想だったが、
+SwingExecute(=ここのクラス)の main() に
+完全修飾クラス名を手打ちして実行するしかなさそう。(無念・・)
+
+ただ、クラス名からコマンドプロンプトを呼び出す cmdDivクラス群は
+ちゃんと機能しているので、今後 使ってみよう。「$1」のところは改良が必要だな。
  */
+
