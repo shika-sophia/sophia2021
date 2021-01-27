a4c2a2ff28fbc8e62cafe9946cbb1c4be500c41a
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-01-27 09:59:28 +0900

    2021-01-27-1000

---
+++ primaryLesson/Lesson003.java
@@ -0,0 +1,28 @@
+/**
+ * @title primaryLesson / primaryAns / Lesson003.java
+ * @content System.out,println(); 「+」演算子と( )
+ * @author shika
+ * @dete 2021-01-27
+ */
+/*
+ * ◆Lesson003
+ *  x = 12, y = 4のとき
+ *  実行結果が次のようになるコードを作成せよ。
+ *  ただし、計算部分は println()の「( )」内に記述すること。
+ *
+ * ====== 実行結果 ======
+ * 12 ＋ 4 ＝ 16
+ * 12 ー 4 ＝ 8
+ * 12 × 4 ＝ 48
+ * 12 ÷ 4 ＝ 3
+ */
+package primaryLesson;
+
+public class Lesson003 {
+
+    public static void main(String[] args) {
+
+
+    }//main()
+
+}//class
