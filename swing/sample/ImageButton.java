38698d5fb4a0dd1a4ef8c441e967510618b369cd
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-01-27 20:29:41 +0900

    2021-01-27-2030

---
+++ swing/sample/ImageButton.java
@@ -0,0 +1,44 @@
+/**
+ * @title javaPractice / swing / sample / ImageButton.java
+ * @refernce 日向俊二『JavaGUIプログラミング
+ *                     ～Swingを使った今どきのアプリ開発』カットシステム, 2020
+ * @content List4.5 / ImageBtn.java
+ * @author shika
+ * @date 2021-01-27
+ */
+package swing.sample;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class ImageButton extends JFrame {
+
+    public ImageButton() {
+        ImageIcon image = new ImageIcon("WebContent/image/iconUN.gif");
+        JButton button = new JButton("Button", image);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ImageIcon image = new ImageIcon("WebContent/image/iconFraulein.jpg");
+                button.setIcon(image);
+            }
+        });
+
+        //JFrame
+        add(button);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Image Button");
+        setSize(520, 240);
+        setVisible(true);
+    }//constructor
+
+    public static void main(String[] args) {
+        new ImageButton();
+    }//main()
+
+}//class
---
+++ swing/sample/SampleButton.java
@@ -0,0 +1,63 @@
+/**
+ * @title javaPractice / swing / sample / SampleButton.java
+ * @refernce 日向俊二『JavaGUIプログラミング』カットシステム, 2020
+ * @content List4.4 / SmplBtn.java
+ * @author shika
+ * @date 2021-01-27
+ */
+package swing.sample;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SampleButton extends JFrame {
+
+    public SampleButton() {
+        JLabel label = new JLabel("ボタンをクリックしてください。");
+        label.setPreferredSize(new Dimension(220, 40));
+
+        JButton button1 = new JButton("ボタン１");
+        button1.setPreferredSize(new Dimension(220, 40));
+        JButton button2 = new JButton("ボタン２");
+        button2.setPreferredSize(new Dimension(220, 40));
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText("ボタン１がクリックされました。");
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText("ボタン２がクリックされました。");
+            }
+        });
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.add(label, BorderLayout.NORTH);
+        panel.add(button1, BorderLayout.CENTER);
+        panel.add(button2, BorderLayout.SOUTH);
+
+        //JFrame
+        add(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Sample Button");
+        pack();
+        setVisible(true);
+    }//constructor
+
+    public static void main(String[] args) {
+        new SampleButton();
+    }//main()
+
+}//class
