31457dc03eb5a3c6c0321989b18feb33883e9cc2
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-02-01 07:31:56 +0900

    2021-02-01-0730

--- javaGold/AnswerEditor.java
+++ javaGold/AnswerEditor.java
@@ -9,7 +9,7 @@ public class AnswerEditor implements InterAnswerSetting {
     public void run() {
         String reference =
             "山本道子『Java Gold SE11 オラクル認定教科書 [1Z0-816]』 翔泳社, 2021 \n"
-          + " *            米山 学 『徹底攻略 Java Gold SE8 問題集[1Z0-809]』 インプレス, 2020";
+          + " *            米山 学 『徹底攻略 Java Gold SE8 問題集 [1Z0-809]』 インプレス, 2020";
 
         ioFileExe.buildJavaDoc(reference);
         ioFileExe.buildFilePage();
@@ -17,15 +17,3 @@ public class AnswerEditor implements InterAnswerSetting {
 
 }//class
 
-/*
-package javaGold.testExamin;
-
-import javaGold.AnswerEditor;
-
-public class TestJavaDoc {
-    public static void main(String[] args) {
-        new AnswerEditor();
-        System.out.println("TestJavaDoc終了");
-    }//main()
-}//class
-*/
--- javaGold/testExamin/TestJavaDoc.java
+++ javaGold/testExamin/TestJavaDoc.java
@@ -1,3 +1,11 @@
+/**
+ * @title javaGold / testExamin / TestJavaDoc.java
+ * @reference 山本道子『Java Gold SE11 オラクル認定教科書 [1Z0-816]』 翔泳社, 2021
+ *            米山 学 『徹底攻略 Java Gold SE8 問題集 [1Z0-809]』 インプレス, 2020
+ * @content
+ * @author shika
+ * @date 2021-02-01
+ */
 package javaGold.testExamin;
 
 import javaGold.AnswerEditor;
@@ -9,19 +17,3 @@ public class TestJavaDoc {
     }//main()
 }//class
 
-/*
-//###### javaGold.testExamin.TestJavaDoc / 2021-02-01 ######
-TestJavaDoc.main() -> new AnswerEditor() ->
-InterAnswerSetting -> new BuildFilePage() の結果
-
-「null
-/＊＊
- * @title javaGold / testExamin / TestJavaDoc.java
- * @reference 山本道子『Java Gold SE11 オラクル認定教科書 [1Z0-816]』 翔泳社, 2021
- *            米山 学 『徹底攻略 Java Gold SE8 問題集[1Z0-809]』 インプレス, 2020
- * @content
- * @author shika
- * @date 2021-02-01
- ＊/
-null 」 となる問題。-> ioFileAnalysis.txtに続く
- */
