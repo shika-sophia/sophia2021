428b96caa512bca1f942f56a8d86cff6347f5933
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-02-01 05:51:29 +0900

    2021-02-01-0600

--- javaGold/testExamin/TestJavaDoc.java
+++ javaGold/testExamin/TestJavaDoc.java
@@ -8,3 +8,20 @@ public class TestJavaDoc {
         System.out.println("TestJavaDoc終了");
     }//main()
 }//class
+
+/*
+//###### javaGold.testExamin.TestJavaDoc / 2021-02-01 ######
+TestJavaDoc.main() -> new AnswerEditor() ->
+InterAnswerSetting -> new BuildFilePage() の結果
+
+「null
+/＊＊
+ * @title javaGold / testExamin / TestJavaDoc.java
+ * @reference 山本道子『Java Gold SE11 オラクル認定教科書 [1Z0-816]』 翔泳社, 2021
+ *            米山 学 『徹底攻略 Java Gold SE8 問題集[1Z0-809]』 インプレス, 2020
+ * @content
+ * @author shika
+ * @date 2021-02-01
+ ＊/
+null 」 となる問題。-> ioFileAnalysis.txtに続く
+ */
