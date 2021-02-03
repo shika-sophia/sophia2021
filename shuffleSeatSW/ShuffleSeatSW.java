c5237600e63280eeb13216613fa3a66e6b20f228
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-02-03 16:16:34 +0900

    2021-02-03-1600

--- shuffleSeatSW/ShuffleSeatSW.java
+++ shuffleSeatSW/ShuffleSeatSW.java
@@ -6,35 +6,56 @@ import java.util.Collections;
 import java.util.List;
 
 public class ShuffleSeatSW {
-    List<String> oldList;
-    List<String> neoList;
+    List<String> oldList; //現在の座席順
+    List<String> neoList; //シャッフル後の座席順
 
     ShuffleSeatSW(){
         oldList = new ArrayList<String>(
-                Arrays.asList("後藤","濱田","中西","門脇","加藤",
-                              "中谷","吉田","藤本","波部","宗像",
-                              "吉見","西岡","西居","赤尾","辻林",
-                              "廣野","大鹿"));
+            Arrays.asList("後藤","濱田","中西","門脇",
+                          "加藤","中谷","吉田","藤本",
+                          "波部","宗像","吉見","西岡",
+                          "西居","赤尾","辻林","廣野"));
         neoList = new ArrayList<String>(oldList);
     }//constructor
 
+    public void shuffleElement() {
+        while(true) {
+            Collections.shuffle(neoList);
+
+            int matched = Arrays.mismatch(
+                oldList.toArray(), neoList.toArray());
+
+            System.out.println("oldList: " + oldList);
+            System.out.println("neoList: " + neoList);
+            System.out.println(matched);
+
+            if(matched == -1) {
+                break;
+            }
+        }//while
+
+    }
     public static void main(String[] args) {
         var shuf = new ShuffleSeatSW();
-        Collections.shuffle(shuf.neoList);
+        shuf.shuffleElement();
 
-        var view = new ViewSeat();
-        view.buildJLabelAry(shuf.oldList);
+//    	var view = new ViewSeat();
+//        view.buildJLabelAry(shuf.oldList);
 
-        //System.out.println("oldList: " + shuf.oldList);
-        //System.out.println("neoList: " + shuf.neoList);
     }//main()
 
 }//class
 
 /*
-oldSeat: [後藤, 濱田, 中西, 門脇, 加藤, 中谷, 吉田, 藤本,
-                  波部, 宗像, 吉見, 西岡, 西居, 赤尾, 辻林, 廣野, 大鹿]
-
-neoSeat: [廣野, 門脇, 辻林, 吉見, 宗像, 濱田, 大鹿, 後藤,
-                   吉田, 西居, 中谷, 赤尾, 西岡, 藤本, 加藤, 中西, 波部]
+int matched = Arrays.mismatch(
+    oldList.toArray(), neoList.toArray());
+oldList: [後藤, 濱田, 中西, 門脇, 加藤, 中谷, 吉田, 藤本, 波部, 宗像, 吉見, 西岡, 西居, 赤尾, 辻林, 廣野]
+neoList: [赤尾, 加藤, 中谷, 廣野, 西居, 波部, 藤本, 吉見, 宗像, 西岡, 吉田, 中西, 濱田, 門脇, 後藤, 辻林]
+0
+oldList: [後藤, 濱田, 中西, 門脇, 加藤, 中谷, 吉田, 藤本, 波部, 宗像, 吉見, 西岡, 西居, 赤尾, 辻林, 廣野]
+neoList: [後藤, 廣野, 波部, 吉田, 中西, 宗像, 西居, 辻林, 加藤, 門脇, 西岡, 赤尾, 藤本, 濱田, 中谷, 吉見]
+1
+【考察】
+mismatch()は違っている indexを出力。完全一致なら -1。
+出したい結果は全て違っているList なので、これじゃない。
 */
