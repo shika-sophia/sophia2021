18592ed116adcc9bbf5ca6278283806ad4af0926
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-02-09 20:24:15 +0900

    2021-02-09-2030

--- javaGold/InterAnswerSetting.java
+++
@@ -1,9 +0,0 @@
-package javaGold;
-
-import utility.ioFileDiv.BuildFilePage;
-import utility.scanDiv.ScanInt;
-
-public interface InterAnswerSetting {
-    BuildFilePage ioFileExe = new BuildFilePage();
-    ScanInt scanExe = new ScanInt();
-}//interface
