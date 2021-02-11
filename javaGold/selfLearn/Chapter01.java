93126be97f45c2adfef6341aa7469bbcf0a1340f
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-02-11 20:33:42 +0900

    2021-02-11-2030

---
+++ javaGold/selfLearn/Chapter01.java
@@ -0,0 +1,9 @@
+package javaGold.selfLearn;
+
+import javaGold.PracticeEditor;
+
+public class Chapter01 {
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+}//class
