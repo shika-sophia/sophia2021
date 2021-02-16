1f625af620ef006be23d6e7d431c117d03db640d
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-02-16 21:57:43 +0900

    2021-02-16-2200

---
+++ primaryLesson/Lesson006.java
@@ -0,0 +1,25 @@
+/**
+ * @title primaryLesson / primaryAns / Lesson006.java
+ * @content 演算子
+ * @author shika
+ * @dete 2021-01-28
+ */
+/*
+ * ◆Lesson006
+ * x = 10, y = 4 のとき、
+ * 10 ÷ 4 の計算結果を 小数で表示せよ。
+ * また 10 ÷ 4 をしたときの余りを表示せよ。
+ *
+ * ◇実行結果
+ * x ÷ y = 2.50
+ * x ÷ y の余りは 2
+ */
+package primaryLesson;
+
+public class Lesson006 {
+
+    public static void main(String[] args) {
+
+    }//main()
+
+}//class
