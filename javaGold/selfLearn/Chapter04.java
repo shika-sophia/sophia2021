ff5e44c7f23b274e6504de6393b9fca0b6a88a62
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-02-17 17:40:09 +0900

    2021-02-17-1730

---
+++ javaGold/selfLearn/Chapter04.java
@@ -0,0 +1,57 @@
+/**
+ * @title javaGold / selfLearn / Chapter04.java
+ * @reference 山田祥寛『独習 Java 新版』 翔泳社, 2019
+ * @content 第4章 制御構文 / ◇練習問題 4.1,  ◇練習問題 4.2,  ◆章末問題
+ * @author shika
+ * @date 2021-02-17
+ */
+/* Appendix
+ * @costTime 13:11 - 13:25 ( 13 分)
+ * @correctRate 正答率 100.00 ％ ( 〇21問 / 全21問 )
+ */
+package javaGold.selfLearn;
+
+import javaGold.PracticeEditor;
+
+public class Chapter04 {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
+/*
+//====== 2021-02-17 ======
+ 第4章 制御構文 / ◇練習問題 4.1
+〇 （1） var rdm = new Random();
+〇 （2） int point = rdm.nextInt(100) + 1;
+〇 （3） String result = "";
+〇 （4） if(point >= 90){
+〇 （5）     result = "優";
+〇 （6） } else if(point >= 70){
+〇 （7）     result = "良";
+〇 （8） } else if(point >= 50){
+〇 （9）     result = "可";
+〇 （10） } else {
+〇 （11）     result = "不可";
+〇 （12） }
+〇 （13） System.out.println("result: " + result);
+
+ ◇練習問題 4.2
+〇 （1） while: 条件判定でtrueなら繰り返し
+〇 （2） do while: 最初に必ず１回は処理をして、その後 条件trueなら繰り返し
+〇 （3） 別紙
+
+ ◆章末問題
+〇 （1） ① for, ② args, ③ value, ④ i
+〇 （2） 別紙
+〇 （3） 別紙
+〇 （4） 別紙
+〇 （5） 別紙
+
+開始時刻 13:11
+終了時刻 13:25
+所要時間 13 分
+正答率 100.00 ％ ( 〇21問 / 全21問 )
+*/
+
---
+++ javaGold/selfLearn/Chapter04_Odd.java
@@ -0,0 +1,31 @@
+/**
+ * @title javaGold / selfLearn / Chapter04_Odd.java
+ * @reference 山田祥寛『独習 Java 新版』 翔泳社, 2019
+ * @content 第4章 制御構文 / ◆章末問題 [3] / p141
+ * @content 題意を変えて、 100-200の奇数の合計をStreamで求める
+ * @content [英] odd: 奇数, even:偶数
+ * @author shika
+ * @date 2021-02-17
+ */
+package javaGold.selfLearn;
+
+import java.util.stream.IntStream;
+
+public class Chapter04_Odd {
+
+    public static void main(String[] args) {
+        int sum = IntStream.rangeClosed(100, 200).sum();
+        int sumOdd = IntStream.rangeClosed(100, 200)
+            .filter(i -> (i % 2 == 1))
+            .sum();
+
+        System.out.println("sum: " + sum);
+        System.out.println("sumOdd: " + sumOdd);
+    }//main()
+
+}//class
+
+/*
+sum: 15150
+sumOdd: 7500
+ */
