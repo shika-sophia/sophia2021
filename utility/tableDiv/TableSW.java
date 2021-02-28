208a5050764a24af1a14d5f7ca5025f5193acc07
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-02-28 18:49:00 +0900

    2021-02-28-1850

--- utility/tableDiv/TableSW.java
+++ utility/tableDiv/TableSW.java
@@ -18,7 +18,6 @@ import java.awt.event.ActionListener;
 import java.awt.print.PrinterException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import javax.swing.JButton;
@@ -49,9 +48,8 @@ public class TableSW
         this.columnAry = columnAry;
         this.valueArys = valueArys;
         super.columnList = Arrays.asList(columnAry);
-        super.valueLists =Arrays.stream(valueArys)
-                .map((Function<String[], List<String>>)
-                    ary -> Arrays.stream(ary).collect(Collectors.toList()))
+        super.valueLists = Arrays.stream(valueArys)
+                .map(ary -> Arrays.stream(ary).collect(Collectors.toList()))
                 .collect(Collectors.toList());
     }
 
@@ -178,38 +176,38 @@ valueArys:
 //        }
 //    }//main()
 //
-//    //====== Test Table(String[], String[][]) ======
-//    public static void main(String[] args) {
-//        String[] columnAry = Stream.of("id", "name", "adress")
-//                            .toArray(String[]::new);
-//        String[] rowAry1 = Stream.of("1", "Alice", "Alaska")
-//                .toArray(String[]::new);
-//        String[] rowAry2 = Stream.of("2", "Bobby", "Britain")
-//                .toArray(String[]::new);
-//        String[] rowAry3 = Stream.of("3", "Charry", "Canada")
-//                .toArray(String[]::new);
-//        String[][] valueArys = Stream.of(rowAry1, rowAry2, rowAry3)
-//                               .toArray(String[][]::new);
+//  //====== Test Table(String[], String[][]) ======
+//  public static void main(String[] args) {
+//      String[] columnAry = Stream.of("id", "name", "adress")
+//                          .toArray(String[]::new);
+//      String[] rowAry1 = Stream.of("1", "Alice", "Alaska")
+//              .toArray(String[]::new);
+//      String[] rowAry2 = Stream.of("2", "Bobby", "Britain")
+//              .toArray(String[]::new);
+//      String[] rowAry3 = Stream.of("3", "Charry", "Canada")
+//              .toArray(String[]::new);
+//      String[][] valueArys = Stream.of(rowAry1, rowAry2, rowAry3)
+//                             .toArray(String[][]::new);
 //
-//        var here = new TableSW(columnAry, valueArys);
+//      var here = new TableSW(columnAry, valueArys);
 //
-//        System.out.println("columnList: " + here.columnList);
-//        System.out.println("valueLists: ");
-//        for(List<String> rowList : here.valueLists) {
-//            rowList.stream().map(s -> s + ", ")
-//                    .forEach(System.out::print);
-//            System.out.println();
-//        }//for
+//      System.out.println("columnList: " + here.columnList);
+//      System.out.println("valueLists: ");
+//      for(List<String> rowList : here.valueLists) {
+//          rowList.stream().map(s -> s + ", ")
+//                  .forEach(System.out::print);
+//          System.out.println();
+//      }//for
 //
-//        //---- Test buildAry() ----
-//        System.out.println("columnAry: " + Arrays.toString(here.columnAry));
-//        System.out.println();
+//      //---- Test buildAry() ----
+//      System.out.println("columnAry: " + Arrays.toString(here.columnAry));
+//      System.out.println();
 //
-//        System.out.println("valueArys: ");
-//        for (String[] rowAry : here.valueArys) {
-//            Arrays.stream(rowAry).map(s -> s + ", ")
-//                    .forEach(System.out::print);
-//            System.out.println();
-//        }//for
-//    }//main()
+//      System.out.println("valueArys: ");
+//      for (String[] rowAry : here.valueArys) {
+//          Arrays.stream(rowAry).map(s -> s + ", ")
+//                  .forEach(System.out::print);
+//          System.out.println();
+//      }//for
+//  }//main()
 */
