f68c48c4646950b9864cf4c12afb7d5b4aa5e941
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-03-04 11:09:53 +0900

    2021-03-04-1100

--- multiThread/chap05MT/arrayBlockingQueue/CakeTableBlocking.java
+++ multiThread/chap05MT/arrayBlockingQueue/CakeTableBlocking.java
@@ -1,15 +1,11 @@
 package multiThread.chap05MT.arrayBlockingQueue;
 
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.TimeUnit;
 
 import multiThread.chap05MT.producerConsumer.CakeTable;
 
-public class CakeTableBlocking
-        extends CakeTable implements BlockingQueue<String> {
+public class CakeTableBlocking extends CakeTable {
     @SuppressWarnings("rawtypes")
     private final BlockingQueue queue;
 
@@ -35,41 +31,5 @@ public class CakeTableBlocking
 
         return cake;
     }//take()
-
-    @Override
-    public String remove() {return null;}
-    public String poll() {return null;}
-    public String element() {return null;}
-    public String peek() {return null;}
-    public int size() {return 0;}
-    public boolean isEmpty() {return false;}
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    public Iterator iterator() {return null;}
-    public Object[] toArray() {return null;}
-    @SuppressWarnings("unchecked")
-    public Object[] toArray(Object[] a) {return null;}
-    @SuppressWarnings("rawtypes")
-    public boolean containsAll(Collection c) {return false;}
-    @SuppressWarnings("rawtypes")
-    public boolean addAll(Collection c) {return false;}
-    @SuppressWarnings("rawtypes")
-    public boolean removeAll(Collection c) {return false;}
-    @SuppressWarnings("rawtypes")
-    public boolean retainAll(Collection c) {return false;}
-    public void clear() { }
-    public boolean add(String e) {return false;}
-    public boolean offer(String e) {return false;}
-    //public void put(Object e) throws InterruptedException { }
-    public boolean offer(String e, long timeout, TimeUnit unit)
-            throws InterruptedException {return false;}
-    public String poll(long timeout, TimeUnit unit)
-            throws InterruptedException {return null;}
-    public int remainingCapacity() {return 0;}
-    public boolean remove(Object o) {return false;}
-    public boolean contains(Object o) {return false;}
-    @SuppressWarnings("rawtypes")
-    public int drainTo(Collection c) {return 0;}
-    @SuppressWarnings("rawtypes")
-    public int drainTo(Collection c, int maxElements) {return 0;}
 
 }//class
