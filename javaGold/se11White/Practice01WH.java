42142fc2dbfb5671d09302df7207be1124be32d7
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-03-09 08:33:50 +0900

    2021-03-09-0830

---
+++ javaGold/se11White/Practice01WH.java
@@ -0,0 +1,19 @@
+/**
+ * @title javaGold / se11White / Practice01WH.java
+ * @reference 日本サードパーティ  『Java Gold SE11 オラクル認定問題集 [1Z0-816]』 翔泳社, 2021
+ * @content 第１章 Javaの基本
+ * @author shika
+ * @date 2021-03-09
+ */
+package javaGold.se11White;
+
+import utility.ioFileDiv.BuildFilePage;
+
+public class Practice01WH {
+
+    public static void main(String[] args) {
+        new BuildFilePage("第１章 Javaの基本");
+    }//main()
+
+}//class
+
