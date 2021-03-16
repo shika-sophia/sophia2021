c75743a1a388d9a53b3b0930675d5f7242d5a096
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-03-16 16:42:42 +0900

    2021-03-16-1700

--- multiThread/chap09MT/webReader/MainWebReader.java
+++ multiThread/chap09MT/webReader/MainWebReader.java
@@ -50,3 +50,39 @@ public class MainWebReader {
 
 }//class
 
+/*
+Getting 45286 bytes form http://www.hyuki.com/
+Getting 45287 bytes form http://www.hyuki.com/
+  :
+Getting 46909 bytes form http://www.hyuki.com/
+Getting 46910 bytes form http://www.hyuki.com/
+Getting 46911 bytes form http://www.hyuki.com/
+java.io.EOFException
+    at java.base/java.io.DataInputStream.readUnsignedByte(DataInputStream.java:295)
+    at multiThread.chap09MT.webReader.content.SyncContentImpl.<init>(SyncContentImpl.java:22)
+    at multiThread.chap09MT.webReader.content.Retriever.retrieve(Retriever.java:5)
+    at multiThread.chap09MT.webReader.MainWebReader.main(MainWebReader.java:24)
+java.io.FileNotFoundException: multiThread\cho09MT\content\yahoo.html (指定されたパスが見つかりません。)
+    at java.base/java.io.FileOutputStream.open0(Native Method)
+    at java.base/java.io.FileOutputStream.open(FileOutputStream.java:298)
+    at java.base/java.io.FileOutputStream.<init>(FileOutputStream.java:237)
+    at java.base/java.io.FileOutputStream.<init>(FileOutputStream.java:126)
+    at multiThread.chap09MT.webReader.MainWebReader.saveToFile(MainWebReader.java:37)
+    at multiThread.chap09MT.webReader.MainWebReader.main(MainWebReader.java:26)
+java.io.FileNotFoundException: multiThread\cho09MT\content\google.html (指定されたパスが見つかりません。)
+    at java.base/java.io.FileOutputStream.open0(Native Method)
+    at java.base/java.io.FileOutputStream.open(FileOutputStream.java:298)
+    at java.base/java.io.FileOutputStream.<init>(FileOutputStream.java:237)
+    at java.base/java.io.FileOutputStream.<init>(FileOutputStream.java:126)
+    at multiThread.chap09MT.webReader.MainWebReader.saveToFile(MainWebReader.java:37)
+    at multiThread.chap09MT.webReader.MainWebReader.main(MainWebReader.java:27)
+java.io.FileNotFoundException: multiThread\cho09MT\content\hyuki.html (指定されたパスが見つかりません。)
+    at java.base/java.io.FileOutputStream.open0(Native Method)
+    at java.base/java.io.FileOutputStream.open(FileOutputStream.java:298)
+    at java.base/java.io.FileOutputStream.<init>(FileOutputStream.java:237)
+    at java.base/java.io.FileOutputStream.<init>(FileOutputStream.java:126)
+    at multiThread.chap09MT.webReader.MainWebReader.saveToFile(MainWebReader.java:37)
+    at multiThread.chap09MT.webReader.MainWebReader.main(MainWebReader.java:28)
+Elapsed time: 4147 msec
+
+*/
