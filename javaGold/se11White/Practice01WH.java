800794bde30c27fac4588bed08338e426854f609
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-03-16 16:41:13 +0900

    2021-03-16-1700

--- javaGold/se11White/Practice01WH.java
+++ javaGold/se11White/Practice01WH.java
@@ -3,17 +3,45 @@
  * @reference 日本サードパーティ  『Java Gold SE11 オラクル認定問題集 [1Z0-816]』 翔泳社, 2021
  * @content 第１章 Javaの基本
  * @author shika
- * @date 2021-03-09
+ * @date 2021-03-16
  */
+/* Appendix 2021-03-16
+ * @costTime 16:03 - 16:23 ( 19 分)
+ * @correctRate 正答率 77.78 ％ ( 〇7問 / 全9問 )
+ */
+
 package javaGold.se11White;
 
-import utility.ioFileDiv.BuildFilePage;
+import javaGold.PracticeEditor;
 
 public class Practice01WH {
 
     public static void main(String[] args) {
-        new BuildFilePage("第１章 Javaの基本");
+        new PracticeEditor();
     }//main()
 
 }//class
+
+/*
+//====== 2021-03-16 ======
+ 第１章 Javaの基本
+〇 （1） C, E
+〇 （2） C 16行目 empNoは private
+〇 （3） C, D
+〇 （4） A, C
+Ｘ （5） A -> C
+    => static と フィールドは オーバライドに関係なく、
+    変数型のメソッドが呼び出される
+
+〇 （6） B
+Ｘ （7） E => D
+----------ここから
+〇 （8） E
+〇 （9） A
+
+開始時刻 16:03
+終了時刻 16:23
+所要時間 19 分
+正答率 77.78 ％ ( 〇7問 / 全9問 )
+*/
 
