d181b2f387f1a9dcc81aa3a9cb7f744bb0d587ac
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-03-17 11:00:31 +0900

    Delete Retriever.java

--- multiThread/chap09MT/webReader/content/Retriever.java
+++
@@ -1,7 +0,0 @@
-package multiThread.chap09MT.webReader.content;
-
-public class Retriever {
-    public static AbsContent retrieve(String urlStr) {
-        return new SyncContentImpl(urlStr);
-    }//retrieve()
-}//class
