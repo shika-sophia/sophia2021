feb4599e40eb40fdd92f1988e5decb9002d95ad5
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-04-02 12:11:38 +0900

    2021-04-02-1200

--- javaGold/se11Violet/sampleCode/chap04VT/MainGeneric.java
+++ javaGold/se11Violet/sampleCode/chap04VT/MainGeneric.java
@@ -5,7 +5,7 @@
  * @reference DJ: 山田祥寛『独習 Java 新版』 翔泳社, 2019
  * @content 第９章 Generics / p453 / List 9.44, 9.45
  * @content DJ-453には、<? extends T>に add()不可とある。
- *          YM-154には、add()可とある。どちらが正しいか検証。
+ *          YM-158には、<? super T> の Tだけは add()可とあるのを検証。
  * @author shika
  * @date 2021-04-01
  */
@@ -24,6 +24,16 @@ public class MainGeneric {
             System.out.println(p.getName());
         }
     }//show()
+
+    public void display(List<? super Child> list) {
+        //list.add(new Parent("波平"));
+        list.add(new Child("カツオ"));
+
+        for(Object ch : list) {
+            System.out.println(((Child) ch).getName());
+        }
+    }//display()
+
     public static void main(String[] args) {
         List<Parent> parentList = new ArrayList<>(
             Arrays.asList(
@@ -40,9 +50,22 @@ public class MainGeneric {
             )
         );
 
+        @SuppressWarnings("unused")
+        List<Friend> friendList = new ArrayList<>(
+            Arrays.asList(
+                new Friend("イクラ"),
+                new Friend("妙子")
+            )
+        );
+
         var here = new MainGeneric();
         here.show(parentList);
         here.show(childList);
+        //here.display(parentList); //ClassCastException
+        here.display(childList);
+        //here.display(friendList);
+        //型 MainGeneric のメソッド display(List<? super Child>) は
+        //引数 (List<Friend>) に適用できません
     }//main()
 
 }//class
@@ -64,6 +87,12 @@ class Child extends Parent {
         super(name);
     }
 }//class Child
+
+class Friend extends Child {
+    Friend(String name) {
+        super(name);
+    }
+}//class Friend
 
 /*
 //====== show(List<Parent>) ======
@@ -106,5 +135,27 @@ list.add()するとコンパイルエラーになる。
 の問題をジェネリックに持ち込まないため。
 配列の共変によって、Object[]に String, Integerなどが代入でき、
 型安全が崩れる問題を ジェネリックで解決したかったから導入された仕様。
+
+//====== display(List<? super Child> list) ======
+ワカメ
+タラ
+カツオ
+
+＊for文で各要素の Childインスタンスを取り出そうとしたが、うまくいかない。
+<? extends Parent>から Parentはできたのだが・・・
+    for(Child ch : list) {
+        System.out.println(ch.getName());
+    }
+    -> 型の不一致: 要素タイプ <? super Child> から Child には変換できません
+    for(Object ch : list) {
+        System.out.println(((Child) ch).getName());
+    }
+
+＊display(parentList)は 実行時に ClassCastException
+＊display(friendList)は コンパイルエラー。
+    -> 型 MainGeneric のメソッド display(List<? super Child>) は
+             引数 (List<Friend>) に適用できません
+
+＊list.add(new Child("カツオ")); は成功
 
 */
