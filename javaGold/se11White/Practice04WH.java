125674b588226223887e655682fad8364b00fb61
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-04-02 12:13:20 +0900

    2021-04-02-1200

---
+++ javaGold/se11White/Practice04WH.java
@@ -0,0 +1,11 @@
+package javaGold.se11White;
+
+import javaGold.PracticeEditor;
+
+public class Practice04WH {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
