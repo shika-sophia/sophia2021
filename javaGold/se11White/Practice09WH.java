b2edc52d228da1f7e1957183abe86824d8ecfc89
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-04-10 12:04:59 +0900

    2021-04-10-1200

---
+++ javaGold/se11White/Practice09WH.java
@@ -0,0 +1,54 @@
+/**
+ * @title javaGold / se11White / Practice09WH.java
+ * @reference 日本サードパーティ 『Java Gold SE11 オラクル認定問題集 [1Z0-816]』 翔泳社, 2021
+ * @content 第９章 ioFile, nioFiles
+ * @author shika
+ * @date 2021-04-10
+ */
+/* Appendix 2021-04-10
+ * @costTime 10:56 - 11:37 ( 40 分)
+ * @correctRate 正答率 68.18 ％ ( 〇15問 / 全22問 )
+ */
+package javaGold.se11White;
+
+import javaGold.PracticeEditor;
+
+public class Practice09WH {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
+/*
+//====== 2021-04-10 ======
+ 第９章 ioFile, nioFiles
+〇 （1） D
+〇 （2） B
+〇 （3） E
+〇 （4） B
+〇 （5） C
+〇 （6） A
+〇 （7） C
+Ｘ （8） B
+〇 （9） C
+〇 （10） A
+Ｘ （11） C
+Ｘ （12） D
+Ｘ （13） D?
+Ｘ （14） A?
+〇 （15） E?
+〇 （16） C
+〇 （17） A
+Ｘ （18） A
+〇 （19） E
+〇 （20） C
+Ｘ （21） A, D
+〇 （22） B
+
+開始時刻 10:56
+終了時刻 11:37
+所要時間 40 分
+正答率 68.18 ％ ( 〇15問 / 全22問 )
+*/
+
