6ca47414c0b33451a77438e71e4a044d36263f4b
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-04-13 11:40:26 +0900

    2021-04-13-1130

---
+++ javaGold/se11Violet/Practice11VT.java
@@ -0,0 +1,11 @@
+package javaGold.se11Violet;
+
+import javaGold.PracticeEditor;
+
+public class Practice11VT {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
