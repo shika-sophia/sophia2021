9efaa7f486a937f4fe398c6e655cdd9ff0a84fd9
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-04-20 18:01:17 +0900

    2021-04-20-1800

---
+++ javaGold/se11Violet/sampleCode/chap06VT/MainCollectorsAggregate.java
@@ -0,0 +1,122 @@
+/**
+ * @title javaGold / se11Violet / sampleCode / chap06VT / MainCollectorsAggregate.java
+ * @reference 山本道子 『Java Gold SE11 オラクル認定教科書 [1Z0-816]』 翔泳社, 2021
+ * @content 第６章 StreamAPI / p247 / List 6-27, 6-20, 6-28
+ * @content ◆Collectorsファクトリメソッド / 集計関数
+ *          ＊static mapping()
+ *          戻値: Collector<T, ?, R>
+ *          引数: mapping(Function<? super T, ? extends U> mapper,
+ *                        Collector<? super U,A,R> downstream )
+ *
+ *          ＊static summingInt()
+ *          戻値: Collector<T, ?, Integer>
+ *          引数: summingInt(ToIntFunction<? super T>)
+ *
+ *          ＊static averagingInt()
+ *          戻値: Collector<T, ?, Double>
+ *          引数: averagingInt(ToIntFunction<? super T>)
+ *
+ *          ＊static maxBy()
+ *          戻値: Collector<T, ?, Optional<T>>
+ *          引数: maxBy(Comparator<? super T>)
+ *
+ *          ＊static minBy()
+ *          戻値: Collector<T, ?, Optional<T>>
+ *          引数: minBy(Comparator<? super T>)
+ *
+ * @author shika
+ * @date 2021-04-20
+ */
+package javaGold.se11Violet.sampleCode.chap06VT;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class MainCollectorsAggregate {
+
+    public static void main(String[] args) {
+        List<String> list =
+            Stream.of("naoki","akko","ami")
+            .collect(Collectors.toList());
+        System.out.println("list: " + list);
+
+        //---- mapping() ----
+        String upper =
+            list.stream()
+                .collect(
+                    Collectors.mapping(s -> s.toUpperCase(),
+                    Collectors.joining(" / ") )
+                );
+        System.out.println("upper: " + upper);
+
+        List<Integer> lengthList =
+                list.stream()
+                .collect(
+                        Collectors.mapping(s -> s.length(),
+                                Collectors.toList())
+                        );
+        System.out.println("lengthList: " + lengthList);
+
+        //---- summingInt() ----
+        Integer sum =
+            list.stream()
+            .collect(
+                Collectors.summingInt(s -> s.length())
+            );
+        System.out.println("sum: " + sum);
+
+        //---- averagingInt() ----
+        Double average =
+            list.stream()
+            .collect(
+                Collectors.averagingInt(s -> s.length())
+            );
+        System.out.println("average: " + average);
+
+        //---- minBy() ----
+        Optional<String> minStr =
+            list.stream()
+                .collect(
+                    Collectors.minBy(Comparator.naturalOrder())
+                );
+        System.out.println("minStr: " + minStr.get());
+
+        //---- maxBy() ----
+        Map<String, Optional<String>> groupMax =
+            IntStream.of(101, 102, 105, 203, 207)
+            .mapToObj(i -> String.valueOf(i))
+            .collect(
+                Collectors.groupingBy(
+                    s -> s.substring(0,1),
+                    Collectors.maxBy(Comparator.naturalOrder())
+                )
+            );
+        System.out.println("groupMax: " + groupMax);
+    }//main()
+
+}//class
+
+/*
+//---- mapping() ----
+list: [naoki, akko, ami]
+upper: NAOKI / AKKO / AMI
+lengthList: [5, 4, 3]
+
+//---- summingInt() ----
+sum: 12
+
+//---- averagingInt() ----
+average: 4.0
+
+//---- minBy() ----
+minStr: akko
+
+//---- maxBy() ----
+groupMax: {1=Optional[105], 2=Optional[207]}
+
+*/
--- javaGold/se11Violet/sampleCode/chap06VT/MainCollectorsFactory.java
+++ javaGold/se11Violet/sampleCode/chap06VT/MainCollectorsFactory.java
@@ -1,7 +1,7 @@
 /**
  * @title javaGold / se11Violet / sampleCode / chap06VT / MainCollectorsFactory.java
  * @reference 山本道子 『Java Gold SE11 オラクル認定教科書 [1Z0-816]』 翔泳社, 2021
- * @content 第６章 StreamAPI
+ * @content 第６章 StreamAPI / p252 / List 6-24, 6-25
  * @reference 山田祥寛『独習 Java 新版』 翔泳社, 2019
  * @content 10.2.4 StreamAPI 終端処理 / p505 / List 10-47 ～ 10-50
  * @content ◆Collectorsファクトリーメソッド -> 戻値: Collectorオブジェクトを生成
@@ -19,12 +19,19 @@
  *          引数: groupingBy(Function<? super T, ? extends K> classifier,
  *                           Collector<? super T,A,D> downstream )
  *
+ *          戻値: Collector<T, ?, M>
+ *          引数: groupingBy(Function<? super T, ? extends K> classifier,
+ *                           Supplier<M> mapFactory
+ *                           Collector<? super T,A,D> downstream )
+ *
  *          T: 入力要素の型(Streamの要素型)
  *          K: グループ化の key
  *          A: 引数 downstreamでの中間型
  *          D: 引数 downstreamでの結果型
+ *          M: 引数 mapFactoryの要素型
  *          classifier: キーによって値を分類するための式
  *          downstream: グループ化後の処理を表すコレクター
+ *          mapFactory: 結果用のMapを生成
  *
  *          ＊static partitioningBy()
  *          戻値: Collector<T, ?, Map<Boolean, List<T>>
@@ -36,6 +43,9 @@
 package javaGold.se11Violet.sampleCode.chap06VT;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -47,27 +57,27 @@ public class MainCollectorsFactory {
             .collect(Collectors.toList());
 
         //---- joining() ----
-        System.out.println(
+        String joined =
             data.stream()
                 .sorted()
-                .collect(Collectors.joining(",", "<", ">"))
-        );
+                .collect(Collectors.joining(",", "<", ">"));
+        System.out.println(joined);
 
         //---- groupingBy(Function<T,K>) ----
         data.set(data.indexOf("チューリップ"), "さざんか");
         data.add("うめ");
         System.out.println("data: " + data);
 
-        System.out.println(
+        Map<Integer,List<String>> groupMap1 =
             data.stream()
                 .sorted()
                 .collect(
                     Collectors.groupingBy(str -> str.length())
-                )
-        );
+                );
+        System.out.println(groupMap1);
 
       //---- groupingBy(Function<T,K>, Collector<T,?,Map<K,D>) ----
-        System.out.println(
+        Map<Integer,String> groupMap2 =
             data.stream()
                 .sorted()
                 .collect(
@@ -75,18 +85,60 @@ public class MainCollectorsFactory {
                         str -> str.length(),
                         Collectors.joining("/")
                     )
-                )
-        );
+                );
+        System.out.println(groupMap2);
 
       //---- partitioningBy(Predicate<? super T>) ----
-        System.out.println(
+        Map<Boolean, List<String>> mapBoolean =
             data.stream()
                 .sorted()
                 .collect(
                     Collectors.partitioningBy(
                         str -> str.length() > 3)
-                )
-        );
+                );
+            System.out.println(mapBoolean);
+
+        //====== YM: List 6-24, 6-25 ======
+        //---- groupingBy(Function<T,K>) ----
+        List<String> data2 =
+            Stream.of("bobby","akiko","ami","belle","nao")
+            .collect(Collectors.toList());
+
+        Map<String, List<String>> groupMap3 =
+            data2.stream()
+                .collect(Collectors.groupingBy(s -> s.substring(0, 1)));
+        System.out.println(groupMap3);
+
+      //---- groupingBy(Function<T,K>, Collector<T,?,Map<K,D>) ----
+        Map<String,Set<String>> groupMap4 =
+            data2.stream()
+                .collect(
+                    Collectors.groupingBy(
+                        s -> s.substring(0, 1),
+                        Collectors.toSet())
+                );
+        System.out.println(groupMap4);
+
+        Map<String,String> groupMap5 =
+            data2.stream()
+                .collect(
+                    Collectors.groupingBy(
+                        s -> s.substring(0, 1),
+                        Collectors.joining("/"))
+                );
+        System.out.print(groupMap5.getClass().getSimpleName());
+        System.out.println(groupMap5);
+
+        Map<String,String> groupMap6 =
+            data2.stream()
+                .collect(
+                    Collectors.groupingBy(
+                        s -> s.substring(0, 1),
+                        TreeMap::new,
+                        Collectors.joining("/"))
+                );
+        System.out.print(groupMap6.getClass().getSimpleName());
+        System.out.println(groupMap6);
     }//main()
 
 }//class
@@ -106,4 +158,8 @@ data: [バラ, さくら, さざんか, あさがお, ゆり, うめ]
 //---- partitioningBy(Predicate<? super T>) ----
 {false=[うめ, さくら, ゆり, バラ], true=[あさがお, さざんか]}
 
+//---- YM: List 6-24, 6-25 ----
+{a=[akiko, ami], b=[bobby, belle], n=[nao]}
+HashMap{a=akiko/ami, b=bobby/belle, n=nao}
+TreeMap{a=akiko/ami, b=bobby/belle, n=nao}
 */
