68935c6e0d1e7517d37bdf43cca495d3aa84328e
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-04-21 11:57:55 +0900

    2021-04-21-1200

---
+++ javaGold/testExamin/TestCtrlJ.java
@@ -0,0 +1,41 @@
+/**
+ * @title javaGold / testExamin / TestCtrlJ.java
+ * @reference なし
+ * @content  Ctrl + Jの実験
+ * @author shika
+ * @date 2021-04-21
+ */
+/* Appendix 2021-04-21
+ * @costTime 11:27 - 11:27 ( 0 分)
+ * @correctRate 正答率 50.00 ％ ( 〇1問 / 全2問 )
+ */
+package javaGold.testExamin;
+
+import javaGold.PracticeEditor;
+
+public class TestCtrlJ {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
+/*
+//====== 2021-04-21 ======
+ Ctrl + Jの実験
+〇 （1） A
+Ｘ （2） C
+
+開始時刻 11:27
+終了時刻 11:27
+所要時間 0 分
+正答率 50.00 ％ ( 〇1問 / 全2問 )
+*/
+
+/*
+Eclipseの設定を変更し [Ctrl] + [J] (= javaで実行)で起動したところ
+javaDoc,Appendixが書き込めない不具合があったが
+ちゃんと書き込めている。(再現できなかった)
+
+あれは何だったのか・・
+*/
