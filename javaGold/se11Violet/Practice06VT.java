eff2979fa1abbe4fa60125dda6300d67eb551d57
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-04-21 11:58:35 +0900

    2021-04-21-1200

--- javaGold/se11Violet/Practice06VT.java
+++ javaGold/se11Violet/Practice06VT.java
@@ -1,11 +1,53 @@
+/**
+ * @title javaGold / se11Violet / Practice06VT.java
+ * @reference 山本道子 『Java Gold SE11 オラクル認定教科書 [1Z0-816]』 翔泳社, 2021
+ * @content  第６章 StreamAPI
+ * @author shika
+ * @date 2021-04-21
+ */
+/* Appendix 2021-04-21
+ * @costTime ※修正入力のため計測できず 11:19 - 11:21 ( 2 分)
+ * @correctRate 正答率 40.00 ％ ( 〇8問 / 全20問 )
+ */
 package javaGold.se11Violet;
 
 import javaGold.PracticeEditor;
 
 public class Practice06VT {
 
-	public static void main(String[] args) {
-		new PracticeEditor();
-	}//main()
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
 
 }//class
+
+/*
+//====== 2021-04-21 ======
+ 第６章 StreamAPI
+Ｘ （1） B, D
+〇 （2） C, E, F
+〇 （3） F
+〇 （4） E, F
+Ｘ （5） F
+〇 （6） E
+Ｘ （7） C
+Ｘ （8） B, D
+Ｘ （9） E
+Ｘ （10） A
+〇 （11） B, F
+〇 （12） E
+〇 （13） D
+Ｘ （14） B, C, F
+Ｘ （15） B
+Ｘ （16） A
+Ｘ （17） E
+Ｘ （18） A, B
+Ｘ （19） B, C
+〇 （20） D
+
+開始時刻 11:19
+終了時刻 11:21
+所要時間 2 分
+正答率 40.00 ％ ( 〇8問 / 全20問 )
+*/
+
