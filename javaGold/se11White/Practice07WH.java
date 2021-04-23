8b86366b29f17a17d53fcd38981d26608203ba1c
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-04-23 11:44:33 +0900

    2021-04-23-1200

---
+++ javaGold/se11White/Practice07WH.java
@@ -0,0 +1,45 @@
+/**
+ * @title javaGold / se11White / Practice07WH.java
+ * @reference 日本サードパーティ 『Java Gold SE11 オラクル認定問題集 [1Z0-816]』 翔泳社, 2021
+ * @content  第７章 モジュール
+ * @author shika
+ * @date 2021-04-23
+ */
+/* Appendix 2021-04-23
+ * @costTime 11:29 - 11:39 ( 9 分)
+ * @correctRate 正答率 10.00 ％ ( 〇1問 / 全10問 )
+ */
+package javaGold.se11White;
+
+import javaGold.PracticeEditor;
+
+public class Practice07WH {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
+/*
+//====== 2021-04-23 ======
+ 第７章 モジュール
+Ｘ （1） D
+〇 （2） D
+Ｘ （3） ?
+Ｘ （4） C
+Ｘ （5） B
+Ｘ （6） ?
+Ｘ （7） ?
+Ｘ （8） C
+Ｘ （9） ?
+Ｘ （10） ?
+
+開始時刻 11:29
+終了時刻 11:39
+所要時間 9 分
+正答率 10.00 ％ ( 〇1問 / 全10問 )
+
+まだ問題をやるレベルに達していない。
+ちゃんと勉強してから再度挑戦すべし。
+*/
+
