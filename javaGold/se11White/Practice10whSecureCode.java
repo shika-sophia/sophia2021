b0f4e885343f6e123b2d467aadd30b04c91ecdf0
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-05-01 12:54:22 +0900

    2021-05-01-1300

---
+++ javaGold/se11White/Practice10whSecureCode.java
@@ -0,0 +1,11 @@
+package javaGold.se11White;
+
+import javaGold.PracticeEditor;
+
+public class Practice10whSecureCode {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
