060fd5f44d12e9133c8e383040dd84484bed46ac
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-05-04 09:55:40 +0900

    2021-05-04-1000

---
+++ javaGold/se08Black/Practice03bkFunctionLambda.java
@@ -0,0 +1,62 @@
+/**
+ * @title javaGold / se08Black / Practice03bkFunctionLambda.java
+ * @reference 米山 学 『徹底攻略 Java Gold SE8 問題集 [1Z0-809]』 インプレス, 2020
+ * @content  第３章 Function, Lambda
+ * @author shika
+ * @date 2021-05-04
+ */
+/* Appendix 2021-05-04
+ * @costTime 08:48 - 09:00 ( 12 分)
+ * @correctRate 正答率 94.44 ％ ( 〇17問 / 全18問 )
+ */
+package javaGold.se08Black;
+
+import javaGold.PracticeEditor;
+
+public class Practice03bkFunctionLambda {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
+/*
+//====== 2021-05-04 ======
+ 第３章 Function, Lambda
+〇 （1） B, C, F
+〇 （2） B
+   => Functional Interface
+   => SAM: Single Abstrarct Method
+
+   => java.nio.file.PathMacher 関数型インターフェイス
+        boolen matches(Path)
+
+〇 （3） D, E
+〇 （4） D
+Ｘ （5） A -> B
+    => 匿名クラスの実装を簡潔に記述。
+           私の回答理由は匿名クラスだけではない。と思ったが確かに間違っていない。
+    => 第１級オブジェクト、高級関数、関数型プログラミング、ラムダの歴史
+
+〇 （6） C
+〇 （7） B
+〇 （8） C, D
+〇 （9） B, D
+〇 （10） A, C, E, F
+〇 （11） A
+〇 （12） D
+〇 （13） B
+〇 （14） C
+〇 （15） D
+〇 （16） B
+    => UnaryOprerator:[英] 単項演算子
+    => BinaryOperator:[英] 二項演算子
+〇 （17） B
+〇 （18） B
+
+開始時刻 08:48
+終了時刻 09:00
+所要時間 12 分
+正答率 94.44 ％ ( 〇17問 / 全18問 )
+*/
+
