559b0eede2fda685e66a3f58bf006432370c1e67
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-05-13 12:04:53 +0900

    2021-05-13-1200

---
+++ javaGold/se08Black/Practice10bkLocale.java
@@ -0,0 +1,46 @@
+/** 
+ * @title javaGold / se08Black / Practice10bkLocale.java 
+ * @reference 米山 学 『徹底攻略 Java Gold SE8 問題集 [1Z0-809]』 インプレス, 2020
+ * @content  第10章 Locale
+ * @author shika 
+ * @date 2021-05-13 
+ */ 
+/* Appendix 2021-05-13 
+ * @costTime 11:19 - 11:29 ( 9 分) 
+ * @correctRate 正答率 78.57 ％ ( 〇11問 / 全14問 )
+ */ 
+package javaGold.se08Black;
+
+import javaGold.PracticeEditor;
+
+public class Practice10bkLocale {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
+/* 
+//====== 2021-05-13 ====== 
+ 第10章 Locale
+〇 （1） C   
+Ｘ （2） A, C, E   
+〇 （3） D   
+〇 （4） C   
+〇 （5） A   
+Ｘ （6） D, E   
+〇 （7） D   
+Ｘ （8） B,D   
+〇 （9） B   
+〇 （10） D   
+〇 （11） D   
+〇 （12） E   
+〇 （13） B   
+〇 （14） A   
+
+開始時刻 11:19
+終了時刻 11:29
+所要時間 9 分
+正答率 78.57 ％ ( 〇11問 / 全14問 )
+*/ 
+
