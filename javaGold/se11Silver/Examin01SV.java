86655983db8d162ff6af24fabb8721f940ad2668
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-05-18 11:33:14 +0900

    2021-05-18-1130

---
+++ javaGold/se11Silver/Examin01SV.java
@@ -0,0 +1,112 @@
+/** 
+ * @title javaGold / se11Silver / Examin01SV.java 
+ * @reference 志賀澄人 『徹底攻略 Java SE11 Silver 問題集[1ZO-815]』 インプレス, 2019
+ * @content  Silver総合問題１
+ * @author shika 
+ * @date 2021-05-18 
+ */ 
+/* Appendix 2021-05-18 
+ * @costTime 09:28 - 11:25 ( 116 分) 
+ * @correctRate 正答率 85.00 ％ ( 〇68問 / 全80問 )
+ */ 
+package javaGold.se11Silver;
+
+import javaGold.PracticeEditor;
+
+public class Examin01SV {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
+/* 
+//====== 2021-05-18 ====== 
+ Silver総合問題１
+Ｘ （1） C   
+〇 （2） A   
+〇 （3） C   
+〇 （4） E   
+〇 （5） C   
+Ｘ （6） A, C   
+Ｘ （7） B   
+〇 （8） E   
+Ｘ （9） A, B   
+〇 （10） B   
+〇 （11） A   
+〇 （12） D   
+〇 （13） D   
+〇 （14） B   
+〇 （15） B   
+〇 （16） D   
+〇 （17） A, E   
+〇 （18） D   
+〇 （19） D   
+〇 （20） D   
+〇 （21） A, E   
+〇 （22） A   
+〇 （23） A, C, E   
+〇 （24） D   
+〇 （25） C   
+〇 （26） D   
+〇 （27） B?   
+Ｘ （28） A, B, E    
+〇 （29） A, D   
+〇 （30） C   
+〇 （31） C   
+〇 （32） A   
+Ｘ （33） B   
+〇 （34） A   
+〇 （35） A   
+〇 （36） A   
+〇 （37） C, E   
+〇 （38） D   
+〇 （39） B   
+〇 （40） C   
+〇 （41） C?   
+〇 （42） C   
+〇 （43） D   
+Ｘ （44） D   
+〇 （45） B   
+〇 （46） A   
+〇 （47） A   
+〇 （48） A   
+〇 （49） A   
+〇 （50） D   
+〇 （51） A, C,    
+〇 （52） C   
+Ｘ （53） B   
+Ｘ （54） C   
+〇 （55） A, B, D, F   
+〇 （56） B   
+〇 （57） C   
+〇 （58） A   
+〇 （59） A   
+Ｘ （60） E   
+Ｘ （61） B   
+〇 （62） A   
+〇 （63） C   
+〇 （64） C   
+〇 （65） A   
+〇 （66） D   
+〇 （67） A   
+〇 （68） A, E   
+〇 （69） D   
+〇 （70） C   
+〇 （71） D   
+〇 （72） B   
+〇 （73） B   
+〇 （74） D   
+〇 （75） B   
+〇 （76） B   
+Ｘ （77） D   
+〇 （78） A, D   
+〇 （79） A, B    
+〇 （80） A   
+
+開始時刻 09:28
+終了時刻 11:25
+所要時間 116 分
+正答率 85.00 ％ ( 〇68問 / 全80問 )
+*/ 
+
