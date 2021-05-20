ca716bf72568b1bb104527ab45fbcda3b00ea229
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-05-20 11:41:00 +0900

    2021-05-20-1200

---
+++ javaGold/se11Violet/Examin01VT.java
@@ -0,0 +1,112 @@
+/** 
+ * @title javaGold / se11Violet / Examin01VT.java 
+ * @reference 山本道子 『Java Gold SE11 オラクル認定教科書 [1Z0-816]』 翔泳社, 2021
+ * @content  Violet総合問題１
+ * @author shika 
+ * @date 2021-05-20 
+ */ 
+/* Appendix 2021-05-20 
+ * @costTime 08:37 - 11:27 ( 169 分) 
+ * @correctRate 正答率 71.25 ％ ( 〇57問 / 全80問 )
+ */ 
+package javaGold.se11Violet;
+
+import javaGold.PracticeEditor;
+
+public class Examin01VT {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
+/* 
+//====== 2021-05-20 ====== 
+ Violet総合問題１
+〇 （1） D   
+〇 （2） A, D   
+〇 （3） B   
+〇 （4） B   
+〇 （5） F   
+Ｘ （6） E, F   
+〇 （7） C   
+Ｘ （8） F   
+Ｘ （9） A, E, F   
+Ｘ （10） B, E   
+Ｘ （11） C   
+〇 （12） A, B   
+〇 （13） F   
+〇 （14） E   
+Ｘ （15） G   
+〇 （16） E, F?   
+Ｘ （17） B, C?   
+〇 （18） F   
+〇 （19） A, B, D, E   
+Ｘ （20） F?   
+〇 （21） A, E, F   
+〇 （22） E   
+Ｘ （23） B, D, E   
+〇 （24） B, D   
+〇 （25） A   
+Ｘ （26） C?   
+〇 （27） B, F   
+〇 （28） B   
+〇 （29） C   
+〇 （30） D   
+Ｘ （31） C?   
+Ｘ （32） C   
+〇 （33） D   
+Ｘ （34） A   
+〇 （35） D, E   
+〇 （36） A   
+〇 （37） C   
+〇 （38） C   
+〇 （39） D   
+〇 （40） C?   
+〇 （41） E   
+Ｘ （42） A, D   
+〇 （43） F   
+Ｘ （44） E   
+〇 （45） D   
+〇 （46） C   
+〇 （47） D   
+〇 （48） C   
+〇 （49） B   
+〇 （50） A   
+〇 （51） D   
+〇 （52） B   
+Ｘ （53） B, D   
+Ｘ （54） A   
+〇 （55） C   
+〇 （56） E   
+〇 （57） E, F   
+〇 （58） F   
+〇 （59） A, E   
+〇 （60） C   
+Ｘ （61） C   
+Ｘ （62） D, E, F   
+〇 （63） E   
+Ｘ （64） E?   
+Ｘ （65） B   
+〇 （66） B, C, D   
+〇 （67） G   
+Ｘ （68） D, E   
+〇 （69） B, E   
+〇 （70） C   
+〇 （71） E   
+〇 （72） D   
+〇 （73） C, D   
+〇 （74） C   
+〇 （75） E   
+〇 （76） B   
+〇 （77） A   
+〇 （78） A   
+〇 （79） E   
+Ｘ （80） A   
+
+開始時刻 08:37
+終了時刻 11:27
+所要時間 169 分
+正答率 71.25 ％ ( 〇57問 / 全80問 )
+*/ 
+
