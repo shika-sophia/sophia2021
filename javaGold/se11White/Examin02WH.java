88e6153e8cd83249c7dec01b32719e5bbdf795af
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-05-22 13:22:05 +0900

    2021-05-22-1330

---
+++ javaGold/se11White/Examin02WH.java
@@ -0,0 +1,112 @@
+/** 
+ * @title javaGold / se11White / Examin02WH.java 
+ * @reference 日本サードパーティ 『Java Gold SE11 オラクル認定問題集 [1Z0-816]』 翔泳社, 2021
+ * @content  White 総合問題２
+ * @author shika 
+ * @date 2021-05-22 
+ */ 
+/* Appendix 2021-05-22 
+ * @costTime 10:05 - 13:07 ( 182 分) 
+ * @correctRate 正答率 68.75 ％ ( 〇55問 / 全80問 )
+ */ 
+package javaGold.se11White;
+
+import javaGold.PracticeEditor;
+
+public class Examin02WH {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
+/* 
+//====== 2021-05-22 ====== 
+ White 総合問題２
+〇 （1） A   
+Ｘ （2） C, D   
+〇 （3） B   
+〇 （4） C, D   
+Ｘ （5） D   
+〇 （6） B   
+〇 （7） B   
+〇 （8） A   
+〇 （9） C   
+Ｘ （10） A   
+Ｘ （11） A   
+Ｘ （12） Ax   
+〇 （13） B   
+〇 （14） D   
+〇 （15） B?   
+〇 （16） D   
+Ｘ （17） C   
+〇 （18） E   
+〇 （19） C   
+〇 （20） C   
+Ｘ （21） B   
+〇 （22） B   
+〇 （23） B   
+〇 （24） D   
+〇 （25） B   
+〇 （26） D   
+〇 （27） D   
+〇 （28） A   
+〇 （29） C   
+〇 （30） B   
+Ｘ （31） A   
+〇 （32） C   
+〇 （33） A   
+〇 （34） A   
+〇 （35） C   
+〇 （36） C   
+〇 （37） C   
+〇 （38） A, D, E   
+〇 （39） C   
+〇 （40） D   
+Ｘ （41） C, D?   
+〇 （42） A   
+Ｘ （43） A   
+〇 （44） B   
+〇 （45） D   
+〇 （46） B, C   
+Ｘ （47） C   
+〇 （48） A, D   
+〇 （49） C   
+〇 （50） C   
+Ｘ （51） C?   
+〇 （52） F   
+〇 （53） D   
+Ｘ （54） Dx   
+〇 （55） B, C   
+〇 （56） B   
+〇 （57） A   
+Ｘ （58） A, C   
+Ｘ （59） B   
+〇 （60） B, D   
+〇 （61） B   
+Ｘ （62） E   
+Ｘ （63） B   
+Ｘ （64） A, B?   
+Ｘ （65） D   
+〇 （66） B   
+〇 （67） A, B   
+〇 （68） B, C   
+〇 （69） C   
+Ｘ （70） A   
+〇 （71） C   
+〇 （72） C   
+〇 （73） B, E   
+Ｘ （74） A   
+〇 （75） A   
+Ｘ （76） E?   
+Ｘ （77） A   
+〇 （78） B   
+Ｘ （79） D   
+Ｘ （80） C   
+
+開始時刻 10:05
+終了時刻 13:07
+所要時間 182 分
+正答率 68.75 ％ ( 〇55問 / 全80問 )
+*/ 
+
