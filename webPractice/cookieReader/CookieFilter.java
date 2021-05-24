fbd05db3840894d24e645bbc42ff202e1fcd490c
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-05-24 21:01:12 +0900

    2021-05-24-2100

--- webPractice/cookieReader/CookieFilter.java
+++ webPractice/cookieReader/CookieFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-@WebFilter("/*")
+@WebFilter("/Cookie")
 public class CookieFilter implements Filter {
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
