601008a1fd889bb76dba65c4c084df3ab7b87061
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-06-03 09:53:48 +0900

    2021-06-03-1000

---
+++ javaGoF/chap09Bridge/Practice09JG.java
@@ -0,0 +1,11 @@
+package javaGoF.chap09Bridge;
+
+import javaGold.PracticeEditor;
+
+public class Practice09JG {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
