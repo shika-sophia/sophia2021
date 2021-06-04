4f76d05de4fc74a84837df1a631b8262ba126ac3
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-06-04 10:39:43 +0900

    2021-06-04-1030

--- javaGoF/chap09Bridge/MainBridge.java
+++ javaGoF/chap09Bridge/MainBridge.java
@@ -4,23 +4,51 @@
  * @content 第９章 Bridge / List 9-1 ～ 9-5
  * @content 機能の階層と実装の階層を分離し、
  *          フィールド impl に実装の階層をのせ、Bridge(=橋)とする。
+ * @content 練習問題 9-1 / RandomDisplayを追加
+ * @content 練習問題 9-2 / ReadFileDisplayImplを追加。
+ *          SimpleFileDisplayImplの引数なしコンストラクタを追加。
+ *          同.setStr(String)を追加し、
+ *          表示する文字列 strをインスタンス時ではなく、
+ *          後から setStr()できるように変更。
+ *          ReadFileDisplayImplで読み込んだ文字列は、
+ *          ReadFile extends Simpleにすることで、枠線を付けて表示。
+ * @content 練習問題 9-3 / DrawDisplayImplを追加。
+ *          CountDisplay.gradualIncrease()を追加。
+ *          AbsDisplayの各メソッドを Overrideする必要があるので、
+ *          superはAbsDisplay。
+ *          CountDisplay.multiDisplay()だと
+ *          <*****>と表示されるので、
+ *          逓増していくようにするため gradualIncrease()を追加。
+ *
+ *          あっ、最大数は固定値ではなく、ランダムなので
+ *          gradualIncrease()は、RandomDisplayに追加。
+ *          ランダム数だけ返すメソッドを分離し、
+ *          randomDisplay()と gradualIncrease()で共有。
  *
  * @class MainBridge / ◆main()
  * @package ---- bridgeFunction / 機能の階層 ----
  * @class BridgeDisplay / impl / open(),print(),close(),display()
  * @class CountDisplay extends BridgeDisplay / multiDisplay()
+ * @class RandomDisplay extends CountDisplay
+ *        / randomReturn(), randomDisplay(), gradualIncrease()
  *
  * @package ---- bridgeImplementation / 実装の階層 ----
  * @class AbsDisplayImpl
- * @class SimpleDisplayImpl
+ * @class SimpleDisplayImpl extends AbsDisplayImpl
+ * @class ReadFileDisplayImpl extends SimpleDisplayImpl
+ * @file  textData.txt
+ * @class DrawDisplayImpl extends AbsDisplay
  *
  * @author shika
- * @date 2021-06-03
+ * @date 2021-06-03, 06-04
  */
 package javaGoF.chap09Bridge;
 
 import javaGoF.chap09Bridge.bridgeFunction.BridgeDisplay;
 import javaGoF.chap09Bridge.bridgeFunction.CountDisplay;
+import javaGoF.chap09Bridge.bridgeFunction.RandomDisplay;
+import javaGoF.chap09Bridge.bridgeImplementation.DrawDisplayImpl;
+import javaGoF.chap09Bridge.bridgeImplementation.ReadFileDisplayImpl;
 import javaGoF.chap09Bridge.bridgeImplementation.SimpleDisplayImpl;
 
 public class MainBridge {
@@ -32,11 +60,21 @@ public class MainBridge {
                 new SimpleDisplayImpl("Hello World"));
         CountDisplay d3 = new CountDisplay(
                 new SimpleDisplayImpl("Hello Universe"));
+        RandomDisplay d4 = new RandomDisplay(
+                new SimpleDisplayImpl("Hello Random"));
+        BridgeDisplay d5 = new BridgeDisplay(
+                new ReadFileDisplayImpl("textData.txt"));
 
         d1.display();
         d2.display();
         d3.display();
         d3.multiDisplay(5);
+        d4.randomDisplay(5);
+        d5.display();
+
+        RandomDisplay d6 = new RandomDisplay(
+                new DrawDisplayImpl());
+        d6.gradualIncrease(8);
     }//main()
 
 }//class
@@ -58,6 +96,47 @@ public class MainBridge {
 |Hello Universe|
 |Hello Universe|
 +--------------+
++------------+
+|Hello Random|
+|Hello Random|
+|Hello Random|
++------------+
++------------+
+|Hello Random|
+|Hello Random|
++------------+
++------------+
+|Hello Random|
+|Hello Random|
+|Hello Random|
+|Hello Random|
++------------+
++--------------+
+|Hello TextFile|
++--------------+
+
+マークを入力してください。 * / # -> *
+<>
+<*>
+<**>
+<***>
+<****>
+<*****>
+<******>
+
+マークを入力してください。 * / # -> #
+|-
+|#-
+|##-
+|###-
+
+マークを入力してください。 * / # -> 8
+<!> 「*」か「#」で入力してください。
+マークを入力してください。 * / # -> -1
+<!> 「*」か「#」で入力してください。
+マークを入力してください。 * / # -> &
+<!> 「*」か「#」で入力してください。
+マークを入力してください。 * / # -> #
 
  */
 
