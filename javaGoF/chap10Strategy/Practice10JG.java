5442ac51419dfcf050dbaeece4efaea559a14d5f
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-06-05 11:36:50 +0900

    2021-06-05-1130

---
+++ javaGoF/chap10Strategy/Practice10JG.java
@@ -0,0 +1,11 @@
+package javaGoF.chap10Strategy;
+
+import javaGold.PracticeEditor;
+
+public class Practice10JG {
+
+    public static void main(String[] args) {
+        new PracticeEditor();
+    }//main()
+
+}//class
