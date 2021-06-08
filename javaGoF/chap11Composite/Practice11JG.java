b7768e55d4f1167eecacbbb8f2dbb579c3acc902
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-06-08 16:05:13 +0900

    2021-06-08-1600

--- javaGoF/chap11Composite/Practice11JG.java
+++ javaGoF/chap11Composite/Practice11JG.java
@@ -1,3 +1,14 @@
+/** 
+ * @title javaGoF / chap11Composite / Practice11JG.java 
+ * @reference 結城 浩 『Java言語で学ぶデザインパターン入門 [増補改訂版]』 SB Creative, 2004
+ * @content  第11章 Composite / 練習問題 11-1, 11-2
+ * @author shika 
+ * @date 2021-06-08 
+ */ 
+/* Appendix 2021-06-08 
+ * @costTime 13:39 - 13:41 ( 1 分) 
+ * @correctRate 正答率 50.00 ％ ( 〇1問 / 全2問 )
+ */ 
 package javaGoF.chap11Composite;
 
 import javaGold.PracticeEditor;
@@ -9,3 +20,17 @@ public class Practice11JG {
     }//main()
 
 }//class
+/* 
+//====== 2021-06-08 ====== 
+ 第11章 Composite / 練習問題 11-1
+Ｘ （1） Swingなど、ツリー構造のクラスをポリモーフィズムするんかな   
+
+ 11-2
+〇 （1） absolutePath()を追加。   
+
+開始時刻 13:39
+終了時刻 13:41
+所要時間 1 分
+正答率 50.00 ％ ( 〇1問 / 全2問 )
+*/ 
+
