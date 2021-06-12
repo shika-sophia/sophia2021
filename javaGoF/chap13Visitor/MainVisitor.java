5b6352abbed27540807a875bedf17804d7b19a3c
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-06-12 12:09:59 +0900

    2021-06-12-1200

--- javaGoF/chap13Visitor/MainVisitor.java
+++ javaGoF/chap13Visitor/MainVisitor.java
@@ -1,10 +1,12 @@
 /**
  * @title javaGoF / chap13Visitor / MainVisitor.java
  * @reference 結城 浩 『Java言語で学ぶデザインパターン入門 [増補改訂版]』 SB Creative, 2004
- * @content 第13章 Visitor / List 13-1 ～ 13-8
+ * @content 第13章 Visitor / List 13-1 ～ 13-8, 練習問題 13-1
  * @content データ構造と処理の分離。entryElementは Composite改。
  *          Compositeの printList()の処理を
  *          Directory.accept() -> ShowVisitor.visit()で行う
+ * @content 練習問題 13-1
+ *          FindVisitorを追加。検索語を含むファイルを抽出。
  *
  * @class MainVisitor / ◆main()
  *
@@ -24,9 +26,12 @@
  * @package ---- visitor ----
  * @class AbsVisitor
  *        / abstract visit(File), visit(Directory)
- * @class ShowVisitor
+ * @class ShowVisitor extends AbsVisitor
  *        / String currentDir /
  *        visit(File), visit(Directory)
+ * @class FindVisitor extends AbsVisitor
+ *        / String findKey, List<AbsEntryVisit> findList /
+ *        visit(File), visit(Directory), getFindList()
  *
  * @author shika
  * @date 2021-06-11
@@ -36,6 +41,8 @@ package javaGoF.chap13Visitor;
 import javaGoF.chap13Visitor.entryElement.DirectoryVisit;
 import javaGoF.chap13Visitor.entryElement.FileTreatException;
 import javaGoF.chap13Visitor.entryElement.FileVisit;
+import javaGoF.chap13Visitor.visitor.FileSizeVisitor;
+import javaGoF.chap13Visitor.visitor.FindVisitor;
 import javaGoF.chap13Visitor.visitor.ShowVisitor;
 
 public class MainVisitor {
@@ -64,10 +71,39 @@ public class MainVisitor {
              userDir.add(taro);
              yuki.add(new FileVisit("JavaGoF.java", 100));
              yuki.add(new FileVisit("MultiThread.java", 200));
-             hanako.add(new FileVisit("diary.txt", 300));
+             hanako.add(new FileVisit("diary.html", 300));
              taro.add(new FileVisit("game.txt", 400));
              taro.add(new FileVisit("gameMemo.html", 500));
+
              rootDir.accept(new ShowVisitor());
+             System.out.println();
+
+             //---- 練習問題 13-1 FindVisitor
+             String findKey = ".html";
+             var finder = new FindVisitor(findKey);
+             rootDir.accept(finder);
+
+             System.out.printf("[%s] files are: \n", findKey);
+             finder.getFindList().forEach(System.out::println);
+             System.out.println();
+
+             //---- 練習問題 13-2 FileSizeVistor ----
+             var sizeVisitor = new FileSizeVisitor();
+             rootDir.accept(sizeVisitor);
+             sizeVisitor.showSize();
+             System.out.println();
+
+             //---- Appendix ----
+             //getFileSize(String)
+             String seekSize = "root";
+             System.out.printf("%s size: %d \n",
+                 seekSize,
+                 sizeVisitor.getFileSize(seekSize));
+
+             //getFileSize(AbsEntryVisit)
+             System.out.printf("%s size: %d \n",
+                 yuki.getName(),
+                 sizeVisitor.getFileSize(yuki));
 
          } catch (FileTreatException e) {
              e.printStackTrace();
@@ -101,4 +137,49 @@ Making user entry...
 /root/user/taro/game.txt (400)
 /root/user/taro/gameMemo.html (500)
 
+//---- 練習問題 13-1 FindVisitor ----
+[.html] files are:
+diary.html (300)
+gameMemo.html (500)
+
+//---- 練習問題 13-2 FileSizeVisitor ----
+//Map<String, Integer> sizeMap
+temp: 0
+yuki: 300
+bin: 30000
+JavaGoF.java: 100
+latex: 20000
+game.txt: 400
+MultiThread.java: 200
+hanako: 300
+vi: 10000
+root: 31500
+gameMemo.html: 500
+diary.html: 300
+taro: 900
+user: 1500
+
+//---- Appendix ----
+root size: 31500
+yuki size: 300
+
+【考察】 練習問題 13-2
+AbsEntry.getName()を keyとした検索用の HashMapなので順不同。
+
+Directory.getSize()を書き換えとの題意だったが、
+entryListを他に持って行って、サイズ集計するぐらいなら、
+そのDirectoryのサイズ集計は
+従来どうりこのDirectoryクラスの getSize()で行ったほうがいいと思う。
+もしDirectory.getSize()を変更するなら、
+ここの処理を FileSizeVisitor.visit(Directory)に委譲する記述にする。
+
+従来通りだと FileSizeVisitorを利用する機会がない。
+表示は AbsEntry.toString(){ getSize() }を利用しているので、
+表示する機会もない。
+FileSizeVisitorは、もともと要らないのでは。
+
+一応 main()で Map一覧を出してみた。
+main()で利用するなら、
+    sizeVisitor.getFileSize(String);
+と記述して、ファイル/ディレクトリ名からサイズを調べる検索用。
 */
