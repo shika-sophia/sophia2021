ca85e4a42eb94435936d894908384e55af45d9a0
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-06-13 12:33:11 +0900

    2021-06-13-1230

--- javaGoF/chap13Visitor/visitor/AbsVisitor.java
+++ javaGoF/chap13Visitor/visitor/AbsVisitor.java
@@ -1,9 +1,19 @@
 package javaGoF.chap13Visitor.visitor;
 
+import javaGoF.chap13Visitor.entryElement.AbsEntryVisit;
 import javaGoF.chap13Visitor.entryElement.DirectoryVisit;
 import javaGoF.chap13Visitor.entryElement.FileVisit;
 
 public abstract class AbsVisitor {
     public abstract void visit(FileVisit file);
     public abstract void visit(DirectoryVisit dir);
+
+    public void visit(AbsEntryVisit entry) {
+        if(entry instanceof FileVisit) {
+            visit((FileVisit) entry);
+        } else if (entry instanceof DirectoryVisit) {
+            visit((DirectoryVisit) entry);
+        }
+    }//visit(AbsEntryVisit)
+
 }//class
