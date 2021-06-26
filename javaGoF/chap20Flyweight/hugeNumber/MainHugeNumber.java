1f21f6952f7fd5fb8cc3e2963a14f6702a48b6a1
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-06-26 18:49:44 +0900

    2021-06-26-1900

--- javaGoF/chap20Flyweight/hugeNumber/MainHugeNumber.java
+++ javaGoF/chap20Flyweight/hugeNumber/MainHugeNumber.java
@@ -1,3 +1,27 @@
+/**
+ * @title javaGoF / chap20Flyweight / hugeNumber / MainHugeNumber.java
+ * @reference 結城 浩 『Java言語で学ぶデザインパターン入門 [増補改訂版]』 SB Creative, 2004
+ * @content 第20章 Flyweight / インスタンスを共有する。
+ *          huge:[英] 巨大な -> hugeStr: 横16 × 縦 8の ascii文字
+ *
+ *          テキスト仕様は txtファイルに元データを書き、
+ *          それを FileReaderで読み込ませて、
+ *          必要に応じて HugeNumberクラスを poolMapで保存する。
+ *
+ * @content 自作 version
+ *          NumberDataに binaryデータの16進数表記で表示のデータ int[]を用意。
+ *          その int[]を 集合した List<int[]>を作る。
+ *          必要に応じて、各 int[]から StringBuilderで hugeStrを作成。
+ *          テキスト仕様と大きく違うクラス構成になってしまった。
+ *
+ * @class MainHugeNumber / ◆main()
+ * @class NumberData
+ * @class HugeNumber
+ * @class HugeNumberFactory
+ *
+ * @author shika
+ * @date 2021-06-26
+ */
 package javaGoF.chap20Flyweight.hugeNumber;
 
 import java.util.List;
@@ -83,3 +107,4 @@ poolMap.size(): 3
 
 
 */
+
