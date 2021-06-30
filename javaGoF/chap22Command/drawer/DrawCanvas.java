fd5a53064727c5da2e226481fef56c3bfb0fa84a
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-06-30 18:31:51 +0900

    2021-06-30-1830

--- javaGoF/chap22Command/drawer/DrawCanvas.java
+++ javaGoF/chap22Command/drawer/DrawCanvas.java
@@ -7,8 +7,8 @@ import java.awt.Graphics;
 import javaGoF.chap22Command.command.MacroCommand;
 
 public class DrawCanvas extends Canvas implements IDrawable {
-    private Color color = Color.RED;//描点の色
-    private int radius = 6;         //描点の半径
+    private Color color;//描点の初期色
+    private int radius; //描点の半径
     private MacroCommand history;
 
     public DrawCanvas(
@@ -16,6 +16,12 @@ public class DrawCanvas extends Canvas implements IDrawable {
         super.setSize(width, height);
         super.setBackground(Color.WHITE);
         this.history = history;
+        init();
+    }
+
+    public void init() {
+        color = Color.RED;
+        radius = 6;
     }
 
     //履歴全体を再描画
@@ -29,5 +35,13 @@ public class DrawCanvas extends Canvas implements IDrawable {
         g.setColor(color);
         g.fillOval(x - radius, y - radius, radius * 2, radius * 2);
     }//draw()
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 
 }//class
--- javaGoF/chap22Command/drawer/IDrawable.java
+++ javaGoF/chap22Command/drawer/IDrawable.java
@@ -1,5 +1,8 @@
 package javaGoF.chap22Command.drawer;
 
+import java.awt.Color;
+
 public interface IDrawable {
     public abstract void draw(int x, int y);
+    public abstract void setColor(Color color);
 }//interface
