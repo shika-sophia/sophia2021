bc0cf0fa7034e56f0271a337afefc3ee82a562b9
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-07-03 12:26:39 +0900

    2021-07-03-1230

--- javaGoF/chap23Interpreter/Practice23JG.java
+++ javaGoF/chap23Interpreter/Practice23JG.java
@@ -12,8 +12,8 @@ public class Practice23JG {
 
 /*
 (1) 練習問題 23-1 / ミニ言語の実行プログラム
-    => サンプル parseNodeパッケージは、ミニ言語を解析するプログラム。
+    => 問 サンプル parseNodeパッケージは、ミニ言語を解析するプログラム。
     *  それを実行して、GUIに表示する。
-    *  => practiceNodeパッケージに記述。
+    => practiceNodeパッケージに記述。
 
 */
