f445f9d1686321c7e20b95ebdd6c1ef8417f4257
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-07-09 09:17:58 +0900

    2021-07-09-0900

--- webPractice/loginAuthenticate/annotationAuth/AuthAnootationServlet.java
+++ webPractice/loginAuthenticate/annotationAuth/AuthAnootationServlet.java
@@ -1,3 +1,15 @@
+/**
+ * @title webPractice / loginAuthenticate / annotationAuth / AuthAnnotationServlet.java
+ * @reference 山田祥寛 『独習 Java サーバーサイド編 第２版』 翔泳社, 2013
+ * @content 第７章 Deployment Descriptor / p368 / List 7-19
+ *          アノテーションで form認証の機能を実現する
+ *          <security-constraint>要素を代替する。
+ *          フォルダ単位の管理には、web.xmlが必要。
+ *          ページ単位のアクセス規制ならアノテーションで十分
+ *
+ * @author shika
+ * @date 2021-07-08
+ */
 package webPractice.loginAuthenticate.annotationAuth;
 
 import java.io.IOException;
