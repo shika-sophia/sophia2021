59a95ffffdb44d0068ea047bef819d8ed442ba50
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-07-12 15:52:19 +0900

    2021-07-12-1600

---
+++ webPractice/listener/contextListener/MyServlet.java
@@ -0,0 +1,30 @@
+/**
+ * @see ExtendListener
+ * @content 上記Listenerで利用するサンプル用のServlet
+ */
+
+package webPractice.listener.contextListener;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("動的に登録されたServlet");
+    }//doGet()
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+}//class
